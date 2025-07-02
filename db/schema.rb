93be26b23f0fdf696f4324c21532155a957c3eaa
lyuhiroyama <lyuhiroyama@gmail.com>
2025-07-02 21:01:58 +0900

    fix(posts): actually run migration to add user_id to posts table
    - Turns out migration (db:migrate) I thought I ran on my last commit didn't go through, hence this commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_081957) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_114936) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id"
@@ -24,6 +24,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_081957) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_081957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
