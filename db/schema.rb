3a552e4b19a0c43bcbfcc9e258638abca6ce25fd
lyuhiroyama <lyuhiroyama@gmail.com>
2025-07-02 17:20:58 +0900

    feat: migrate database for User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_02_081223) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_02_081957) do
   create_table "comments", force: :cascade do |t|
     t.text "body"
     t.integer "post_id"
@@ -22,6 +22,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_081223) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "country"
+    t.string "prefecture"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
